6ed2072febb10c73fa5608162c3267005096efcb
Judy McGoldrick <jjeanius@verizon.net>
2019-02-06 04:04:27 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,18 +5,18 @@ class PostsController < ApplicationController
     @posts = Post.all
     @authors = Author.all
 
- if !params[:author].blank?
-   @posts = Post.by_author(params[:author])
- elsif !params[:date].blank?
-   if params[:date] == "Today"
-     @posts = Post.from_today
+   if !params[:author].blank?
+     @posts = Post.by_author(params[:author])
+   elsif !params[:date].blank?
+     if params[:date] == "Today"
+       @posts = Post.from_today
+     else
+       @posts = Post.old_news
+     end
    else
-     @posts = Post.old_news
+     @posts = Post.all
    end
- else
-   @posts = Post.all
- end
-end
+  end
 
   def show
     @post = Post.find(params[:id])
