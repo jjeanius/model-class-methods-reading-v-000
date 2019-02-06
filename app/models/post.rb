7f80e554bf4688a0081cba3150444a45834540d9
Judy McGoldrick <jjeanius@verizon.net>
2019-02-06 04:07:58 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,7 @@ class Post < ActiveRecord::Base
   end
 
   def self.old_news
-    where("created_at <?", Time.zone.today.beginning_of_day")
+    where("created_at <?", Time.zone.today.beginning_of_day)
   end
 
 
